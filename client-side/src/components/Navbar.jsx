ea9c0959310d56336ae5ef04b821ce37dc2c46b8
Jojo Duke <45549237+JojoDuke@users.noreply.github.com>
2023-09-22 15:39:09 +0000

    adding navbar

---
+++ client-side/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+function Navbar() {
+  
+    return (
+        <nav className="bg-gray-900 p-4 flex items-center justify-between">
+        {/* Logo */}
+        <div className="text-white">
+
+        </div>
+  
+        {/* Navigation Links */}
+        <div className="text-white text-lg">
+          <ul className="flex space-x-4">
+            <li>PRODUCTS</li>
+            <li>BRANDS</li>
+            <li>DELIVERY MAP</li>
+            <li>ABOUT</li>
+            <li>CONTACT</li>
+            <li>TEAM MEMBER APPLICATION</li>
+          </ul>
+        </div>
+  
+        {/* Sign In and Sign Up Buttons */}
+        <div className="space-x-2">
+          <button className="border border-white text-white rounded-full px-4 py-1">LOG IN</button>
+          <button className="border border-white text-white rounded-full px-4 py-1">SIGN UP</button>
+        </div>
+      </nav>
+    );
+  }
+  
+  export default Navbar;
