1e4ec0ffcb04f42bd2e0eb434886c37b739230c6
JojoDuke <jojoamankwa@gmail.com>
2023-09-24 00:11:49 +0000

    Adding columns

--- client-side/src/components/Footer.jsx
+++ client-side/src/components/Footer.jsx
@@ -3,11 +3,84 @@ import mainLogo from '../assets/main-logo.png';
 
 function Footer() {
   return (
-    <footer className="flex flex-row justify-between items-center px-20 bg-[#131313] text-white p-4 absolute bottom-0 w-full">
+    <footer className="flex flex-row justify-between items-start px-20 py-10 bg-[#131313] text-white bottom-0 w-full">
       <img src={mainLogo} alt="main-logo" />
-      <h2 className='text-[#BD64D8]'>Dr FEELGOOD</h2>
-      <h2>NEW MEMBERS</h2>
-      <h2>SOCIAL MEDIA</h2>
+      
+      <div className="">
+        <h2 className='text-[#BD64D8] font-bold mb-5'>DR FEELGOOD</h2>
+
+        <ul className="">
+          <li className="">
+            <a href="/">
+              PRODUCTS
+            </a>
+          </li>
+          <li className="">
+            <a href="/">
+              BRANDS
+            </a>
+          </li>
+          <li className="">
+            <a href="/">
+              DELIVERY MAP
+            </a>
+          </li>
+          <li className="">
+            <a href="/">
+              ABOUT
+            </a>
+          </li>
+          <li className="">
+            <a href="/">
+              CONTACT US
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h2 className='text-[#BD64D8] font-bold mb-5'>NEW MEMBERS</h2>
+
+        <ul>
+            <li>
+                <a href="/">
+                SIGN IN
+                </a>
+            </li>
+            <li>
+                <a href="/">
+                SIGN UP
+                </a>
+            </li>
+            <li>
+                <a href="/">
+                PRIVACY POLICY
+                </a>
+            </li>
+            <li>
+                <a href="/">
+                TERMS AND CONDITIONS
+                </a>
+            </li>
+        </ul>
+      </div>
+
+      <div>
+        <h2 className='text-[#BD64D8] font-bold mb-5'>SOCIAL MEDIA</h2>
+
+        <ul>
+            <li>
+                <a href="/">
+                    INSTAGRAM
+                </a>
+            </li>
+            <li>
+                <a href="/">
+                    TWITTER
+                </a>
+            </li>
+        </ul>
+      </div>
     </footer>
   );
 }
--- client-side/src/components/Hero.jsx
+++ client-side/src/components/Hero.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import delivery from '../assets/delivery.png';
+import shop from '../assets/shop.png';
+import features from '../assets/features.png';
+import mainLogo from '../assets/main-logo.png';
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div className='py-40'>
+      <div className='text-center flex flex-col justify-center items-center'>
+        <h1 className='text-white text-[26px] font-bold mb-10'>START YOUR SHOPPING EXPERIENCE NOW!</h1>
+
+        {/* Delivery And Pickup */}
+        <div className='flex flex-row justify-center items-center gap-10 mb-20'>
+          <div className='h-[600px] w-[400px] bg-[#54B5D5] rounded-md flex flex-col justify-center items-center'>
+            <img src={delivery} alt="truck-icon" />
+            <p className='text-white text-[20px] font-bold mt-12'>DELIVERY</p>
+          </div>
+          <div className='h-[600px] w-[400px] bg-[#BE63DA] rounded-md flex flex-col justify-center items-center'>
+            <img src={shop} alt="truck-icon" />
+            <p className='text-white text-[20px] font-bold mt-12'>PICK-UP</p>
+          </div>
+        </div>
+
+        {/* MainLogo */}
+        <img src={mainLogo} alt="main logo" width="250px" height="250px"/>
+        <h1 className="text-white text-[24px] mt-20">Featured In</h1>
+
+        {/* Featured In */}
+        <img src={features} alt="main logo" width="200px" height="200px"/>
+
+        {/* The Rest */}
+        <p>P</p>
+      </div>
+    </div>
   )
 }
 
